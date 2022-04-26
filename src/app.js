74932bcf64de9d6a2ed7dad5053def223d7c3a64
Santiago Graviano <santiago.graviano@gmail.com>
2022-04-26 13:46:33 -0300

    Fork and Cluster implementation

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
+require('dotenv/config')
+const cluster = require('cluster')
+const cpus = require('os').cpus().length
 const args = require('./services/args')
 const { server } = require('./server.js')
 const { connectIO } = require('./services/io.js')
 const { initializeMongo } = require('./services/mongo.js')
 const { initializeRedis } = require('./services/redis')
 
-const main = async () => {
+const bootstrap = async () => {
   if (process.env.DB === 'mongo' || process.env.SESSION_DRIVER === 'mongo') {
     await initializeMongo()
   }
@@ -14,10 +17,34 @@ const main = async () => {
   }
 
   connectIO(server)
+}
 
+const startServer = () => {
   server.listen(args.port, () => {
-    console.log(`Listening to port ${ args.port }`)
+    console.log(`Worker with pid ${process.pid} listening to port ${ args.port }`)
   })
 }
 
-main()
+bootstrap()
+
+if (args.mode == 'cluster' &&  cluster.isPrimary) {
+  console.log(`Master #${process.pid} is running`)
+
+  for (let i = 0; i < cpus; i++) {
+    cluster.fork()
+  }
+
+
+  let killedProcesses = 0
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died with code ${code}`)
+    killedProcesses += 1
+
+    if (killedProcesses == cpus) {
+      process.exit()
+    }
+  })
+}
+else {
+  startServer()
+}
